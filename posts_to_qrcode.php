16a4531142da95e96414adfaf174a7f966c7a2dc
promahmudul <mahmudul.coder@gmail.com>
2020-12-26 23:43:35 +0600

    Adding hooks for increasing some features

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -24,9 +24,23 @@ function pqrc_display_qr_code( $content ) {
 	$current_post_id    = get_the_ID();
 	$current_post_title = get_the_title( $current_post_id );
 	$current_post_url   = urlencode( get_the_permalink( $current_post_id ) );
-	$image_src          = sprintf( "https://api.qrserver.com/v1/create-qr-code/?size=185x185&data=%s", $current_post_url );
+	$current_post_type  = get_post_type( $current_post_id );
 
-	$content .= sprintf( "<div class='qrcode'><img src='%s' alt='%s'/> </div>", $image_src, $current_post_title );
+	//Post Type Check
+
+	$excluded_post_types = apply_filters( 'pqrc_excloded_post_types', array() );
+	if ( in_array( $current_post_type, $excluded_post_types ) ) {
+		return $content;
+	}
+
+	//Dimension Hook
+	$dimension = apply_filters( 'pqrc_qrcode_dimension', '185x185' );
+
+	//Image Attributes
+	$image_attributes = apply_filters( 'pqrc_image_attributes', null );
+
+	$image_src = sprintf( "https://api.qrserver.com/v1/create-qr-code/?size=%s&data=%s", $dimension, $current_post_url );
+	$content   .= sprintf( "<div class='qrcode'><img src='%s' %s alt='%s'/> </div>", $image_src, $image_attributes, $current_post_title );
 
 	return $content;
 }
