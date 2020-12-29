e1bfb78d8748ff36169beab992eaa03ceb52666c
promahmudul <mahmudul.coder@gmail.com>
2020-12-30 01:17:30 +0600

    custom filter hook for settings field

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -14,6 +14,24 @@
  * Domain Path:       /languages
  */
 
+$pqrc_countries = array(
+	__( 'Afghanistan', 'posts-to-qrcode' ),
+	__( 'Bangladesh', 'posts-to-qrcode' ),
+	__( 'Bhutan', 'posts-to-qrcode' ),
+	__( 'India', 'posts-to-qrcode' ),
+	__( 'Maldives', 'posts-to-qrcode' ),
+	__( 'Nepal', 'posts-to-qrcode' ),
+	__( 'Pakistan', 'posts-to-qrcode' ),
+	__( 'Sri Lanka', 'posts-to-qrcode' )
+);
+
+function pqrc_init() {
+	global $pqrc_countries;
+	$pqrc_countries = apply_filters( 'pqrc_countries', $pqrc_countries );
+}
+
+add_action( 'init', 'pqrc_init' );
+
 function posts_to_qrcode_textdoamin() {
 	load_plugin_textdomain( 'posts-to-qrcode', false, dirname( __FILE__ ) . "/languages" );
 }
@@ -58,11 +76,45 @@ function pqrc_settings_init() {
 
 	add_settings_field( 'pqrc_height', __( 'QR Code Height', 'posts-to-qrcode' ), 'pqrc_display_field', 'general', 'pqrc_section', array( 'pqrc_height' ) );
 	add_settings_field( 'pqrc_width', __( 'QR Code Width', 'posts-to-qrcode' ), 'pqrc_display_field', 'general', 'pqrc_section', array( 'pqrc_width' ) );
-	add_settings_field( 'pqrc_extra', __( 'Extra Fields', 'posts-to-qrcode' ), 'pqrc_display_field', 'general', 'pqrc_section', array( 'pqrc_extra' ) );
+//	add_settings_field( 'pqrc_extra', __( 'Extra Fields', 'posts-to-qrcode' ), 'pqrc_display_field', 'general', 'pqrc_section', array( 'pqrc_extra' ) );
+	add_settings_field( 'pqrc_select', __( 'Dropdown', 'posts-to-qrcode' ), 'pqrc_display_select_field', 'general', 'pqrc_section' );
+	add_settings_field( 'pqrc_checkbox', __( 'Select Countries', 'posts-to-qrcode' ), 'pqrc_display_checkbox_field', 'general', 'pqrc_section' );
 
 	register_setting( 'general', 'pqrc_height', array( 'sanitize_callback' => 'esc_attr' ) );
 	register_setting( 'general', 'pqrc_width', array( 'sanitize_callback' => 'esc_attr' ) );
-	register_setting( 'general', 'pqrc_extra', array( 'sanitize_callback' => 'esc_attr' ) );
+//	register_setting( 'general', 'pqrc_extra', array( 'sanitize_callback' => 'esc_attr' ) );
+	register_setting( 'general', 'pqrc_select', array( 'sanitized_callback' => 'esc_attr' ) );
+	register_setting( 'general', 'pqrc_checkbox' );
+}
+
+function pqrc_display_checkbox_field() {
+	global $pqrc_countries;
+	$option = get_option( 'pqrc_checkbox' );
+
+	foreach ( $pqrc_countries as $country ) {
+		$selected = '';
+		if ( is_array( $option ) && in_array( $country, $option ) ) {
+			$selected = 'checked';
+		}
+		printf( '<input type="checkbox" name="pqrc_checkbox[]" value="%s" %s/>%s<br>', $country, $selected, $country );
+	}
+
+}
+
+function pqrc_display_select_field() {
+	global $pqrc_countries;
+	$option = get_option( 'pqrc_select' );
+
+
+	printf( '<select id="%s" name="%s">', 'pqrc_select', 'pqrc_select' );
+	foreach ( $pqrc_countries as $country ) {
+		$selected = '';
+		if ( $option == $country ) {
+			$selected = 'selected';
+		}
+		printf( '<option value="%s" %s>%s</option>', $country, $selected, $country );
+	}
+	echo '</select>';
 }
 
 function pqrc_section_callback() {
